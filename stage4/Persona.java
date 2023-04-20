4aca7537c0d624e45d83d58af734f29486b3b3af
eldearribaa <129528433+eldearribaa@users.noreply.github.com>
2023-04-19 22:57:32 -0400

    borrando prints
    borrando prints

--- stage4/Persona.java
+++ stage4/Persona.java
@@ -4,8 +4,7 @@ public class Persona {
 
     public Persona(float x, float y){
         position = new Position(x, y);
-        //delete
-        System.out.print("(");System.out.print(x);System.out.print(", ");System.out.print(y);System.out.print(")"); System.out.println();
+
 
     }
     public Position getPosition(){
